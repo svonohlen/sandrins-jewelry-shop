82b5c0862277df6214e2ed7e4f3b1472ad733284
svonohlen <83403193+svonohlen@users.noreply.github.com>
2022-04-18 16:07:20 -0700

    Login page completed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,76 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import backgroundLogin from "../images/background_login.jpg";
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url(${backgroundLogin}) center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
+const Wrapper = styled.div`
+  padding: 20px;
+  width: 25%;
+  background-color: white;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0px;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: #8b7d80;
+  color: white;
+  cursor: pointer;
+  margin-bottom: 10px;
+`;
+
+const Link = styled.a`
+  margin: 5px 0px;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+`;
 const Login = () => {
   return (
-    <Container>Login</Container>
-  )
-}
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder="username" />
+          <Input placeholder="password" />
 
-export default Login
+          <Button>LOGIN</Button>
+
+          <Link>FORGOT YOUR PASSWORD?</Link>
+          <Link>CREATE A NEW ACCOUNT</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Login;
