857945665dc8d12adf424bea4696601d7d6b09fe
svonohlen <83403193+svonohlen@users.noreply.github.com>
2022-04-18 16:01:35 -0700

    Register page completed (#1)

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,20 +4,53 @@ import backgroundReg from "../images/background_register.jpg";
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  background: (${backgroundReg});
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url(${backgroundReg}) center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  padding: 20px;
+  width: 40%;
+  background-color: white;
+`;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
 
-const Form = styled.form``;
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
 
-const Input = styled.input``;
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
 
-const Agreement = styled.span``;
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: #8b7d80;
+  color: white;
+  cursor: pointer;
+`;
 
 const Register = () => {
   return (
