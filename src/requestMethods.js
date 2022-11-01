67e28de38022306d7c5418d6930c5ee9d3e00f2d
svonohlen <83403193+svonohlen@users.noreply.github.com>
2022-10-31 22:57:27 -0700

    extracted API base url to env, prepared for gh pages release

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
   .currentUser.accessToken;
